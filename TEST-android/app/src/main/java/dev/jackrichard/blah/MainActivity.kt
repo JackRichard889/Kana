8b3da7276cb26fd2df152cbc63f7348cc9cd8b36
= <=>
2022-01-13 12:26:57 -0500

    Changes. 3

--- TEST-android/app/src/main/java/dev/jackrichard/blah/MainActivity.kt
+++ TEST-android/app/src/main/java/dev/jackrichard/blah/MainActivity.kt
@@ -2,6 +2,7 @@ package dev.jackrichard.blah
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import dev.jackrichard.kana.*
 
 val vertexDescriptor = defineDescriptor {
